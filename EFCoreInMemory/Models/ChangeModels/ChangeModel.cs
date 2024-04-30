3a7467b820435b2c36e673451b0aec7b44935c04
Linn Thit <lth1212001@gmail.com>
2024-04-30 20:51:44 +0630

    -

--- EFCoreInMemory/Models/ChangeModels/ChangeModel.cs
+++ EFCoreInMemory/Models/ChangeModels/ChangeModel.cs
@@ -7,7 +7,7 @@ namespace EFCoreInMemory.Models.ChangeModels
 		public static CustomerDataModel Change(this CreateCustomerRequestModel requestModel)
 		{
 			var model = new CustomerDataModel()
-			{
+            {
 				CustomerId = Guid.NewGuid().ToString(),
 				CustomerName = requestModel.CustomerName,
 				Email = requestModel.Email,
